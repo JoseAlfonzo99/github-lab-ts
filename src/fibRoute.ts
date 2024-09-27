dfa473214f9683be0c4898ae01ef8618e59ae107
Felix Arnos <ravl4books@gmail.com>
2024-09-27 14:08:39 -0400

    "No changes made to code, only whitespace and formatting adjustments."

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import fibonacci from "./fib";
 
 export default (req: Request, res: Response) => {
-    const { num } = req.params as { num: string }; // Definir que `num` es un string
-    const fibN = fibonacci(parseInt(num)); // `fibN` es un número
-    let result = `fibonacci(${num}) is ${fibN}`; // `fibN` es numérico
+    const { num } = req.params as { num: string }; 
+    const fibN = fibonacci(parseInt(num)); 
+    let result = `fibonacci(${num}) is ${fibN}`; 
 
     if (fibN < 0) {
         result = `fibonacci(${num}) is undefined`;
